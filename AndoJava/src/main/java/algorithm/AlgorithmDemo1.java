03ba5f1b76e3dd7a43b8465552815341841758f0
javakam <jooybao@foxmail.com>
2020-10-16 13:54:19 +0800

    todo 05.栈

--- AndoJava/src/main/java/algorithm/AlgorithmDemo1.java
+++ AndoJava/src/main/java/algorithm/AlgorithmDemo1.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class AlgorithmDemo1 {
 
     public static void main(String[] args) {
-        s2_2();
+        s4_1();
     }
 
     /*
@@ -181,19 +181,64 @@ public class AlgorithmDemo1 {
     // 假设有任意多张面额为 2 元、3 元、7 元的货币，现要用它们凑出 100 元，求总共有多少种可能性。
     // 数学模型 : 7x + 3y + 2z = 100
     // s2_1 时间复杂度向空间复杂度的转移
-    //todo 2020年10月13日 16:50:22  https://kaiwu.lagou.com/course/courseInfo.htm?courseId=185#/detail/pc?id=3340
     private static void s2_2() {
+        int total = 100;
         int count = 0;
-        int num_7 = 0;
-        for (int i = 0; i <= (100 / 7); i++) {
-            num_7 = i;
-        }
-        for (int j = 0; j <= ((100 - 7 * num_7) / 3); j++) {
-            if ((100 - num_7 * 7 - j * 3 >= 0) && ((100 - num_7 * 7 - j * 3) % 2 == 0)) {
-                count += 1;
+        for (int i = 0; i <= (total / 7); i++) {
+            //1.优化   for (int j = 0; j <= (total / 3); j++) -> for (int j = 0; j <= ((total - i * 7) / 3); j++) 减少比对次数
+            final int left = total - i * 7;
+            for (int j = 0; j <= (left / 3); j++) {
+                if ((left - j * 3 >= 0) && ((left + 3 * j) % 2 == 0)) {
+                    count += 1;
+                }
             }
         }
-        System.out.println(count);
+        System.out.println("count = " + count);
     }
+
+    /*
+    map.containsKey里面里面的循环为什么没有增加时间复杂度?
+    讲师回复： 哈希表采用的是哈希函数计算哈希地址，这里的时间复杂度是O(1)的。
+     */
+
+    /*
+    03 | 增删查：掌握数据处理的基本操作,以不变应万变
+    https://kaiwu.lagou.com/course/courseInfo.htm?courseId=185#/detail/pc?id=3341
+
+    04 | 如何完成线性表结构下的增删查？
+    https://kaiwu.lagou.com/course/courseInfo.htm?courseId=185#/detail/pc?id=3342
+    重点: 链表的翻转、快慢指针的方法
+     */
+    class LinkNode {
+        LinkNode index;
+        LinkNode pre;
+        LinkNode next;
+    }
+
+    //例 1，链表的翻转。给定一个链表，输出翻转后的链表。例如，输入1 ->2 -> 3 -> 4 ->5，输出 5 -> 4 -> 3 -> 2 -> 1。
+    private static void s4_1() {
+
+    }
+
+    //例 2，给定一个奇数个元素的链表，查找出这个链表中间位置的结点的数值。
+    private static void s4_2() {
+    }
+
+    //例 3，判断链表是否有环。如下图所示，这就是一个有环的链表。
+    private static void s4_3() {
+    }
+
+    //练习题
+    //最后我们留一道课后练习题。给定一个含有 n 个元素的链表，现在要求每 k 个节点一组进行翻转，打印翻转后的链表结果。其中，k 是一个正整数，且可被 n 整除。
+    //例如，链表为 1 -> 2 -> 3 -> 4 -> 5 -> 6，k = 3，则打印 321654。我们给出一些提示，这个问题需要使用到链表翻转的算法。
+    private static void s4_4() {
+    }
+
+
+    /*
+    05 | 栈：后进先出的线性表，如何实现增删查？
+    https://kaiwu.lagou.com/course/courseInfo.htm?courseId=185#/detail/pc?id=3343
+     */
+
 
 }
